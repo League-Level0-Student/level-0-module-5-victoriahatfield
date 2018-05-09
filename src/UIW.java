602a84d477a81ac413d72ceb196dcc3d9114f81a
Hatfield.Victoria <Hatfield.Victoria@SEMS-259-31.SEMS259.LAB>
2018-05-09 16:19:02 -0700

    We played with numbers by printing the fibbanacci pattern and having the computer tell us if a

---
+++ src/UIW.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class UIW {
+public static void main(String[] args) {
+	System.out.print("1 1");
+	int num1= 1;
+	int num2= 1;
+	String fibba=JOptionPane.showInputDialog("How many fibonacci sequences.");
+	int fib=Integer.parseInt(fibba);
+	for (int i=0;i<fib;i++){
+		int sum= num1+num2;
+		System.out.print(" "+sum+" ");
+		num1=num2;
+		num2=sum;
+	}
+}
+	
+}
---
+++ src/YoyHaveReachedYourPrime.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+public class YoyHaveReachedYourPrime {
+	public static void main(String[] args) {
+
+		String number = JOptionPane.showInputDialog("Jerabadinka: Give me a number between 1-10.");
+		int numb = Integer.parseInt(number);
+		if (numb == 1) {
+			JOptionPane.showMessageDialog(null, "Jerabadinka: " + number + " is not prime! YOU SHALL PERISH!!");
+			return;
+		}
+		if (numb == 2) {
+			JOptionPane.showMessageDialog(null, "Jerabadinka: " + number + " is prime You may continue");
+			return;
+		}
+		for (int i = 2; i < numb; i ++) {
+			if (numb % i == 0) {
+				JOptionPane.showMessageDialog(null, "Jerabadinka: " + number + " is not prime! YOU SHALL PERISH!!");
+				return;
+			}
+
+		}
+		JOptionPane.showMessageDialog(null, "Jerabadinka: " + number + " is prime You may continue");
+	}
+}
+// 27-9-3-20-15-18-9-1 7-1-20-6-5-9-12-4
